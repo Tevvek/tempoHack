8997818698abd9cf63ddac32fec151abd90f027e
Daniil <daniil.teterevkov@gmail.com>
2016-02-20 11:52:44 +0100

    first get made at controller view made

--- client/controllers/jobVacancies-ctrl.js
+++ client/controllers/jobVacancies-ctrl.js
@@ -1,5 +1,9 @@
 var App = angular.module('tempoTeamApp', []);
 
-App.controller('nuevaAppCtrl', ['$scope', function($scope) {
-	var 
+App.controller('nuevaAppCtrl', ['$scope', function($scope,$http) {
+	$http.get('http://localhost:8080/user/id/jobs').then(function(res) {
+		// do stuff
+	}, function(error){
+		console.log('ops');
+	});
 }]);
