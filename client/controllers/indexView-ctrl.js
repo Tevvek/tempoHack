e83da265cad639b652e38e1830bae0b34dfdaeab
Andrea <anco0403@MacBook-Pro-de-Andrea-2.local>
2016-02-20 16:17:09 +0100

    cosas web

--- client/controllers/indexView-ctrl.js
+++ client/controllers/indexView-ctrl.js
@@ -5,4 +5,14 @@ App.controller('nuevaAppCtrl', ['$scope', function($scope) {
 	$scope.email = "socpresi@gironamola.cat";
 	$scope.phoneNumber = "633849504";
 	$scope.houseAddress = "Avinguda Josep Tarradellas, 19, Barcelona, 08029";
+
+	$http.get('http://localhost:8080/user/56c835f9660eafedb99a409c/jobs').then(function(data, status, headers, config) {
+		$scope.name = data.name;
+		$scope.email = data.email;
+		$scope.phoneNumber = data.phone;
+		$scope.houseAddress = data.address;
+		}
+	}, function(error){
+		console.log('ops');
+	});
 }]);
